8355689700196824279d440f307f2a75d2c5a678
Shigehiro <Shiigehiro@users.noreply.github.com>
2017-04-27 09:03:59 +0200

    Update app.js

--- app.js
+++ app.js
@@ -1,26 +1,45 @@
-var eco = {fish: [], algue: []};
+var mysql = require('mysql2')
+database = mysql.createConnection({
+  host            :'localhost',
+  user            :'root',
+  password        :'t697fyze',
+  database        :'Nodequarium'
+});
+database.connect();
+
 var fish_espece = {0: ["Mérou", "Thon", "Poisson-Clown"], 1: ["Carpe", "Bar", "Sole"]}
 var turn = 0;
 function init() {
-
+  database.query('DELETE FROM fish');
+  database.query('ALTER TABLE fish AUTO_INCREMENT = 1');
+  database.query('DELETE FROM algue');
+  database.query('ALTER TABLE algue AUTO_INCREMENT = 1');
+  let count_fish = 0;
+  let count_algue = 0;
+  let addFish = setInterval(function() {if (count_fish === 10) {clearInterval(addFish)} else RandomFish(); count_fish++}, 150);
+  let addAlgue = setInterval(function() {
+    if (count_algue === 3) {clearInterval(addAlgue); Turn()}
+    else RandomAlgue(); count_algue++
+  }, 200);
 }
 
-function Fish(p1, p2) {
-  this.name = `Fish_${eco.fish.size+1}`;
-  this.carnivore = p1.carnivore ? true : false;
-  this.espece = fish_espece[this.carnivore ? 1 : 0][Math.Floor((Math.random()*3)+1)];
-  this.gender = Math.round(Math.random());
-  this.year = 0;
-  this.hp = 10;
+function RandomFish() {
+  database.query('SELECT * FROM fish ORDER BY id DESC', function(err, rows, fields) {
+    database.query('INSERT INTO fish SET ?', {name: `FISH_${rows[0] ? Number(rows[0].id)+1 : 1}`, carn: Math.round(Math.random()), espece: fish_espece[Math.round(Math.random())][Math.floor(Math.random()*3)], gender: Math.round(Math.random()), year: 0, hp: 10});
+  });
 }
 
-function Algue() {
-  this.name = `Algue_${eco.fish.size+1}`;
-  this.gender = Math.round(Math.random());
-  this.year = 0;
-  this.hp = 10;
+function RandomAlgue() {
+  database.query('SELECT * FROM algue ORDER BY id DESC', function(err, rows, fields) {
+    database.query('INSERT INTO algue SET ?', {name: `ALGUE_${rows[0] ? Number(rows[0].id)+1 : 1}`, year: 0, hp: 5});
+  });
 }
 
+function Algue(year, hp) {
+  database.query('SELECT * FROM algue ORDER BY id DESC', function(err, rows, fields) {
+    database.query('INSERT INTO algue SET ?', {name: `ALGUE_${rows[0] ? Number(rows[0].id)+1 : 1}`, "year": year, "hp": hp});
+  });
+}
 function eat_algue(rand, fish) {
   if (eco.algue[rand].hp <= 2) {
     console.log(`${fish.name} a tué ${eco.algue[rand].name}`);
@@ -34,7 +53,7 @@ function eat_algue(rand, fish) {
 function eat_fish(rand, fish) {
   if (eco.fish[rand].hp <= 4) {
     console.log(`${fish.name} a tué ${eco.fish[rand].name}`);
-    eco.fish[rand] === null;
+    eco.fish[rand] === "";
   } else {
     console.log(`${fish.name} a attaqué ${eco.fish[rand].name} (${eco.fish[rand].hp-4} HP)`);
     eco.fish[rand].hp === eco.fish[rand].hp-4;
@@ -43,43 +62,58 @@ function eat_fish(rand, fish) {
 
 function Turn() {
   console.log(`========== TOUR ${turn} ==========`);
-  for (var i=0; i++; i< eco.algue.size) {
-    eco.algue[i].hp = eco.algue[i].hp+1;
-    eco.algue[i].year = eco.algue[i].year+1;
-    
-    // Algue qui grandit
-    if (eco.algue[i].hp >= 10) eco.algue[i].
-  };
-  console.log(`${eco.algue.size} algues dans l'aquarium`); 
-  for (var i=0; i++; i< eco.fish.size) {
+  turn++
+  database.query('SELECT * FROM fish', function(err, fish, fields) {
+    database.query('SELECT * FROM algue', function(err, algue, fields) {
+      for (var i=0; i<algue.length; i++) {
+        database.query(`UPDATE algue SET hp = ${Number(algue[i].hp)+1} WHERE id = ${algue[i].id}`);
+        database.query(`UPDATE algue SET year = ${Number(algue[i].year)+1} WHERE id = ${algue[i].id}`);
+
+         if (algue[i].hp >= 10) {
+           Algue(0, 5);
+           database.query(`UPDATE algue SET hp = 5 WHERE id = ${algue[i].id}`);
+         }
+      };
+      for (var i=0; i<fish.length; i++) {
+        database.query(`UPDATE fish SET hp = ${Number(fish[i].hp)-1} WHERE id = ${fish  [i].id}`);
+        database.query(`UPDATE fish SET year = ${Number(fish[i].year)+1} WHERE id = ${fish[i].id}`);
+
+        if (Number(fish[i].hp)-1 <= 0) database.query(`DELETE FROM fish WHERE id = ${fish[i].id}`);
+      };
+      console.log(`${algue.length} algues dans l'aquarium`);
+      console.log(`${fish.length} poissons dans l'aquarium`);
+    });
+  });
+  /*for (var i=0; i<eco.fish.length; i++) {
     let fish = eco.fish[i];
-    
+
     if (fish === null) return;
     if (fish.hp === 1) return eco.fish[i] === null;
-    
+
     eco.fish[i].hp = fish.hp-1;
     eco.fish[i].year = fish.year+1;
-    
-    
-    if (fish.hp <= 5 && fish.carnivore) { 
+
+    if (fish.hp <= 5 && fish.carnivore) {
       // Poisson qui mange un autre poisson
-      var rand = Math.Floor(Math.random()*eco.fish.size);
-      while(rand === i) { rand = Math.Floor(Math.random()*eco.fish.size) };
-      
-      return eat_fish(rand, fish);
-    } else if (fish.hp <= 5 && !fish.carnivore) { 
+      var rand = Math.floor(Math.random()*eco.fish.length);
+      while(rand === i) { rand = Math.floor(Math.random()*eco.fish.length) };
+
+      eat_fish(rand, fish);
+    } else if (fish.hp <= 5 && !fish.carnivore) {
       // Poisson qui mange une algue
-      var rand = Math.Floor(Math.random()*eco.algue.size);
-      
-      return eat_algue(rand, fish);
+      var rand = Math.floor(Math.random()*eco.algue.length);
+
+      eat_algue(rand, fish);
     } else {
       // Reproduction
       let mothers = eco.fish.filter(function(obj) {
         return obj.espece === fish.espece
       });
-      let rand = Math.Floor(Math.random()*mothers.size);
+      let rand = Math.floor(Math.random()*mothers.length);
       new Fish(fish, mothers[rand])
     }
-  };
-  console.log(`${eco.fish.size} poissons dans l'aquarium`);
+  };*/
+  var sleep = setTimeout(function() {clearInterval(sleep); Turn()}, 2500);
 }
+
+init();
